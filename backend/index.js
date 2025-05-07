9d28dc64519332bb1f89b732d09233ee5fe39e88
Ishant1546 <ishantchannel154@gmail.com>
2025-05-07 21:49:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ import Chat from "./models/chat.js";
 import UserChats from "./models/userChats.js";
 import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 
+require('dotenv').config();
+
 const port = process.env.PORT || 3000;
 const app = express();
 
